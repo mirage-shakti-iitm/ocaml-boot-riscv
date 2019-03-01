42febe7ff4480d2e9bfa9f81bd8712a428a54434
Malte 'sl33k' Bargholz <malte@screenri.de>
2019-03-01 10:12:34 +0530

    add assert macro to avoid usage of gcc builtin

---
+++ src/assert.h
@@ -0,0 +1,12 @@
+#ifndef __ASSERT_H
+#define __ASSERT_H
+
+#include "print.h"
+
+#define assert(x) do {                                          \
+  if (!(x)) {                                                   \
+     util::printf("Assertion '%s' failed in file/line %s:%s\n",#x, __FILE__, __LINE__);               \
+    while(1);                                                  \
+  } } while(0)                                                  \
+
+#endif
