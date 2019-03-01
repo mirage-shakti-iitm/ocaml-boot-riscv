ddf5f20762662c543b75cb88f75829159b43000f
Malte 'sl33k' Bargholz <malte@screenri.de>
2019-03-01 10:16:54 +0530

    improve trap_vector output

--- src/trap.cc
+++ src/trap.cc
@@ -4,7 +4,15 @@
 #include "htif.h"
 
 extern "C" void trap_handler(uint64_t mcause, uintptr_t context) {
-    util::printf("machine mode: unhandlable trap %ld @ %lx, mtval=%lx \n", mcause, read_csr(mepc), read_csr(mtval));
+    if(mcause > 0 ) {
+        //trap
+        util::printf("ocaml boot: trap (%d) @ 0x%x mtval=0x%x\n", mcause, read_csr(mepc), read_csr(mtval));
+    }
+    else {
+        uint64_t mask =  ~((uint64_t)1 << 63);
+        uint64_t masked = mcause & mask;
+        // irq
+        util::printf("ocaml boot: irq (%d) @ 0x%x mtval=0x%x\n", masked, read_csr(mepc), read_csr(mtval));
     pk::htif_poweroff();
 
 }
