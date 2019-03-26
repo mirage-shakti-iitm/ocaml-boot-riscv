ea4e58573965b3e121fabf065525f135bbd36b49
Malte 'sl33k' Bargholz <malte@screenri.de>
2019-03-26 16:13:37 +0530

    redesign public include structure to support in/out-of-tree builds

--- include/boot.h
+++
@@ -1,4 +0,0 @@
-//complete header
-#include "config.h"
-#include "htif.h"
-#include "print.h"
---
+++ include/ocaml-boot-riscv.h
@@ -0,0 +1,4 @@
+//complete header
+#include "ocaml-boot-riscv/config.h"
+#include "ocaml-boot-riscv/htif.h"
+#include "ocaml-boot-riscv/print.h"
