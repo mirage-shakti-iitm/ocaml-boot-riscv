e4c50b47453b6aacf65ace147a0649e68ea5735c
Malte 'sl33k' Bargholz <malte@screenri.de>
2019-04-05 16:09:52 +0530

    dont recalculate irq mask every time

--- src/trap.cc
+++ src/trap.cc
@@ -9,7 +9,7 @@ extern "C" void trap_handler(uint64_t mcause, uintptr_t context) {
         util::printf("ocaml boot: trap (%d) @ 0x%x mtval=0x%x\n", mcause, read_csr(mepc), read_csr(mtval));
     }
     else {
-        uint64_t mask =  ~((uint64_t)1 << 63);
+        static uint64_t mask =  ~((uint64_t)1 << 63);
         uint64_t masked = mcause & mask;
         // irq
         util::printf("ocaml boot: irq (%d) @ 0x%x mtval=0x%x\n", masked, read_csr(mepc), read_csr(mtval));
