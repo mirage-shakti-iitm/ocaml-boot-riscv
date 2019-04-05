586f3b2c7fd9c9eeb15fbb91882536c5ea13ffee
Malte 'sl33k' Bargholz <malte@screenri.de>
2019-04-05 16:10:55 +0530

    add timer control

---
+++ src/timer.cc
@@ -0,0 +1,20 @@
+#include <ocaml-boot-riscv/timer.h>
+#include <ocaml-boot-riscv/config.h>
+#include <encoding.h>
+
+
+namespace pk {
+    bool timer_pending() {
+        int ip = read_csr(mip);
+        return (ip & MIP_MTIP) > 0;
+    }
+    void set_timer(unsigned long long deadline) {
+        volatile unsigned long *cmp = (volatile unsigned long*)config::mtimecmp;
+        *cmp = deadline;
+    }
+    void clear_timer() {
+        volatile unsigned long *cmp = (volatile unsigned long*)config::mtimecmp;
+        *cmp = 0;
+    }
+
+}
