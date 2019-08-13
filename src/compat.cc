9ee71b2eb7f65deb4b53f565c5b445fda09de673
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-08-13 15:02:11 +0530

    No wfi, output mode to htif, scale down delay (earlier versions scaled down next which was logically wrong

--- src/compat.cc
+++ src/compat.cc
@@ -20,7 +20,7 @@ extern "C" {
 
     void riscv_wait(time__t delay) {
         time__t curr = riscv_clock_monotonic();
-        delay = delay/(100000);
+        delay = delay/(1000000);
         time__t next = curr+delay;
         pk::set_timer(next);
         while(!pk::timer_pending()){
