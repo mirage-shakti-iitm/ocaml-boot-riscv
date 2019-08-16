50181e6f9bb7f5aefadc8797120b8ad127803232
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-08-16 11:57:47 +0530

    Print mip

--- src/timer.cc
+++ src/timer.cc
@@ -1,12 +1,13 @@
 #include <timer.h>
 #include <config.h>
 #include <encoding.h>
-
+#include <print.h>
 
 namespace pk {
     bool timer_pending() {
         int ip = read_csr(mip);
         int ie = read_csr(mie);
+        util::printf("%d",ip);
         return (ip & MIP_MTIP) > 0;
     }
     void set_timer(unsigned long long deadline) {
