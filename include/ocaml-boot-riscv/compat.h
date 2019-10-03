5d000818b18d2bba83356f41c65dcbd0fc6a178c
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-10-03 16:21:16 +0530

    Trial

--- include/ocaml-boot-riscv/compat.h
+++ include/ocaml-boot-riscv/compat.h
@@ -1,11 +1,12 @@
 // functions exposed to ocaml-freestanding
 // these need to not be name mangled
+#include "bits.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 	typedef unsigned long long time__t;
-	typedef unsigned int size_t;
+
     void riscv_poweroff(int status) __attribute__((noreturn));
     void riscv_write(const char* s, unsigned int length);
     size_t riscv_read(char* s, unsigned int length);
