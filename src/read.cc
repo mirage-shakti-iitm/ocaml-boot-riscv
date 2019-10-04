a46a0596adbf0ba96ca9af68642df11e08ed984a
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-10-04 14:53:16 +0530

    Trial

--- src/read.cc
+++ src/read.cc
@@ -10,17 +10,19 @@ namespace util {
     
 size_t getstring(char* s,unsigned int len){
     size_t i = 0;
-    do{
-#if defined(QEMU_UART) || defined(SHAKTI_UART)
-            s[i] = pk::uart_getchar();
-#elif defined(HTIF)
-           s[i] = pk::htif_console_getchar();
-#else
-#error "none of QEMU_UART, SHAKTI_UART or HTIF is defined!"
-#endif
-           i++;
-  } while( s[i-1] != '\0' || s[i-1] != '\n' || (i-1)<10);
-  return i-1;
+    s[i++] = pk::uart_getchar();
+    s[i++] = pk::uart_getchar();
+//     do{
+// #if defined(QEMU_UART) || defined(SHAKTI_UART)
+//             s[i] = pk::uart_getchar();
+// #elif defined(HTIF)
+//            s[i] = pk::htif_console_getchar();
+// #else
+// #error "none of QEMU_UART, SHAKTI_UART or HTIF is defined!"
+// #endif
+//            i++;
+//   } while((i-1)<10);
+  return i;
 }
 
 }
