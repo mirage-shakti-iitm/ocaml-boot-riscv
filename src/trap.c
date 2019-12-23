19c1203757bfaff843c8d6a9037c2e8718ddb59d
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-12-23 10:08:24 +0530

    Changed mtval to mbadaddr
    mtval not defined in riscv-gcc-7 assembler, can use mtval when using clang+llvm because finally we will be using latest clang to generate the object file

--- src/trap.c
+++ src/trap.c
@@ -6,13 +6,14 @@
 void trap_handler(uint64_t mcause, uintptr_t context) {
     if(mcause > 0 ) {
         //trap
-        printf("ocaml boot: trap (%d) @ 0x%x mtval=0x%x\n", mcause, read_csr(mepc), read_csr(mtval));
+        // mtval not defined in riscv-gcc-7 assembler, can use mtval when using clang+llvm because finally we will be using latest clang to generate the object file
+        printf("ocaml boot: trap (%d) @ 0x%x mtval=0x%x\n", mcause, read_csr(mepc), read_csr(mbadaddr)); 
     }
     else {
         static uint64_t mask =  ~((uint64_t)1 << 63);
         uint64_t masked = mcause & mask;
         // irq
-        printf("ocaml boot: irq (%d) @ 0x%x mtval=0x%x\n", masked, read_csr(mepc), read_csr(mtval));
+        printf("ocaml boot: irq (%d) @ 0x%x mtval=0x%x\n", masked, read_csr(mepc), read_csr(mbadaddr));
     }
     htif_poweroff();
 }
