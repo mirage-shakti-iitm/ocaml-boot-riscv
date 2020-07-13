beda0259023956ed0d446da9c74a5122e31b82a0
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-07-13 20:36:28 +0530

    Enabled compile-time generation of capability matrix and appending it to startup_2.c

--- choose_compartment_strategy.sh
+++ choose_compartment_strategy.sh
@@ -2,6 +2,17 @@
 
 option=$1
 chosen_dir="compartment_allocations/strategy_${option}"
+cap_matrix_dir="compartment_strategy_details/strategy_${option}"
 src_dir="src/"
 
+# chosen_dir will contain the *(.cap/cap_tee.ml) files
 cp ${chosen_dir}/* ${src_dir}/
+
+# generate cap_matrix.h in chosen_dir
+cap_matrix_generator ${cap_matrix_dir}/cap_table.txt
+
+# restore original startup_2.c
+cp ${src_dir}/startup_2_org.c ${src_dir}/startup_2.c
+
+# copy cap_matrix to startup_2.c, correct way is to manually add the cap_matrix to startup_2.c
+cat cap_matrix.h >> ${src_dir}/startup_2.c
