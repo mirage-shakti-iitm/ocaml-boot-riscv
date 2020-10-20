2348acfa02511a00d81e911a2415be03a9ddf290
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-10-20 09:53:33 +0530

    Restore run_constructors

--- src/constructors.c
+++ src/constructors.c
@@ -12,11 +12,11 @@
 
     void run_constructors(void) {
         // Zero out the bss segment
-        for (volatile char *p = &_fbss; p != &_ebss; p++) { *p = '\0'; }
+        // for (volatile char *p = &_fbss; p != &_ebss; p++) { *p = '\0'; }
 
-        // calls all constructors between __CTORS_START and __CTORS_END
-        // needs to be aligned by on 8 Byte boundary
-        for( void (** volatile ctor)() = &__CTORS_START; ctor != &__CTORS_END; ++ctor ) {
-            (*ctor)();
-        }
+        // // calls all constructors between __CTORS_START and __CTORS_END
+        // // needs to be aligned by on 8 Byte boundary
+        // for( void (** volatile ctor)() = &__CTORS_START; ctor != &__CTORS_END; ++ctor ) {
+        //     (*ctor)();
+        // }
     }
