8d3ce964096cf6542a5ae56a9f05e5665d4f42ef
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-10-23 12:26:49 +0530

    Fixed error: Undefined reference to putchar

--- src/trap.c
+++ src/trap.c
@@ -22,15 +22,15 @@ void trap_handler(uint64_t mcause, uintptr_t context) {
 void print_checkcap_no_cross_comp_fail() {
     printf("\nCheckcap no cross-comp fail : (%d) to (%d) at 0x%x", read_csr(ucurrcap), read_csr(utargetcap), read_csr(mepc));
     printf("\nCurrcap_PC_Base = (%d)", read_csr(ucurrcappcbase));
-    printf("\nCurrcap_PC_Bound = (%d)", read_csr(ucurrcappcbound));
-    printf("\n");
+    printf("\nCurrcap_PC_Bound = (%d)\n", read_csr(ucurrcappcbound));
+    // printf("\n");
     htif_poweroff();
 }
 
 void print_checkcap_cap_matrix_fail() {
     printf("\nCheckcap capability matrix fail : (%d) to (%d) at 0x%x", read_csr(ucurrcap), read_csr(utargetcap), read_csr(mepc));
     printf("\nCurrcap_PC_Base = (%d)", read_csr(ucurrcappcbase));
-    printf("\nCurrcap_PC_Bound = (%d)", read_csr(ucurrcappcbound));
-    printf("\n");
+    printf("\nCurrcap_PC_Bound = (%d)\n", read_csr(ucurrcappcbound));
+    // printf("\n");
     htif_poweroff();    
 }
