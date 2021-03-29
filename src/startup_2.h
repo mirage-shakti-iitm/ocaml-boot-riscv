acd7b0c789d74c811ac8e246e50c3bf6ca8a64f5
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2021-03-29 21:03:34 +0530

    Removed assumption that application part of nolibc code

--- src/startup_2.h
+++ src/startup_2.h
@@ -4,16 +4,18 @@
 // provided by linker
 extern char __KERNEL_END;
 
-/*
+
 //provided by libasmrun.a
-extern "C" {
-    extern void caml_startup(const char** argv);
-}
+// extern "C" {
+    // extern void caml_startup(const char** argv);
+// }
 
 //provided by libnolibc.a
 extern "C" {
     extern void _nolibc_init(uintptr_t heap_start, uint64_t heap_size);
 }
-*/
+
+//provided by the application
+extern void main();
 
 #endif
--- src/startup_2_org.c
+++ src/startup_2_org.c
@@ -546,15 +546,11 @@ void boot_primary() {
     // boot_printf("\nGanesha\n");
 
     // boot_printf("ocaml-boot: heap@0x%x stack@0x%x\n",start, &stack[stack_size]);
-    /*  
+      
     _nolibc_init(start, mem_size);
-
-    const char *argv[2] = { "ocaml-boot-riscv", nullptr };
-
-    // call ocaml land
-    caml_startup(argv);
-    */
-    riscv_boot_finished(start, mem_size);
+    
+    //Invoke start function of your application
+    main();
 
     // boot_printf("ocaml-boot: caml runtime returned. shutting down!\n");
 // #ifndef UART
