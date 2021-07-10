fe8269c5d9b1613e004fab67d3b59d7430707720
SaiVK <saiganesha5.svkv@gmail.com>
2021-07-10 14:44:56 +0530

    Fixed installation paths

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 # if [ "$prefix" = "" ]; then
     prefix=`opam config var prefix`
 # fi
-DESTINC=${prefix}/include
+DESTINC=${prefix}/riscv-sysroot/include/ocaml-boot-riscv
 # DESTLIB=${prefix}/lib/ocaml-boot-riscv
-DESTLIB=${prefix}/lib
+DESTLIB=${prefix}/riscv-sysroot/lib/ocaml-boot-riscv
 mkdir -p ${DESTINC} ${DESTLIB}
 # mkdir -p ${DESTINC}
 
@@ -16,8 +16,8 @@ cp build/src/libboot.a ${DESTLIB}/libboot.a
 
 # META: ocamlfind and other build utilities test for existance ${DESTLIB}/META
 # when figuring out whether a library is installed
-# touch ${DESTLIB}/META
+touch ${DESTLIB}/META
 
 # pkg-config
-mkdir -p ${prefix}/share/pkgconfig
-cp ocaml-boot-riscv.pc ${prefix}/share/pkgconfig/ocaml-boot-riscv.pc
+mkdir -p ${prefix}/riscv-sysroot/lib/pkgconfig
+cp ocaml-boot-riscv.pc ${prefix}/riscv-sysroot/lib/pkgconfig/ocaml-boot-riscv.pc
