d28fbfe3e0dc42c7b2dbe8658c357a7f3feef9f6
SaiVK <saiganesha5.svkv@gmail.com>
2021-08-21 14:46:40 +0530

    Updated SM and stack size

--- src/defs.h
+++ src/defs.h
@@ -1,8 +1,8 @@
-#define STACK_SIZE 16384
-#define STACK_SHIFT 14
+#define STACK_SIZE 131072
+#define STACK_SHIFT 17
 
 // Compartment specific macros that should be set on entry.
-#define CHECKCAP_STACK_SIZE 4096
+#define CHECKCAP_STACK_SIZE 4096*2
 #define ENABLE_CAP 0x1
 #define INITIAL_COMPARTMENT 0x0
 #define Total_Compartments 256
