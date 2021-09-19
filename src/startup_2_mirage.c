28b4c5664e84bc1c55ef8f0e065d400ce09b8074
SaiVK <saiganesha5.svkv@gmail.com>
2021-09-19 10:37:20 +0530

    Added least-sp csr HPM

--- src/startup_2_mirage.c
+++ src/startup_2_mirage.c
@@ -34,7 +34,8 @@ void boot_primary() {
     // call mirage land
     riscv_boot_finished(start, mem_size);
 
-    boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx ; \n comp_exceptions : 0x%lx ; \n cycles_comp_exceptions : 0x%lx ; \n cycles_hash : 0x%lx ; \n cycles_val : 0x%lx ;", read_csr(mcycle), read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
+    boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx \n comp_exceptions : 0x%lx \n cycles_comp_exceptions : 0x%lx \n cycles_hash : 0x%lx \n cycles_val : 0x%lx", read_csr(mcycle), read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
+    boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
 
     boot_printf("mirage-boot: caml runtime returned. shutting down!\n");
 
--- src/startup_2_ocaml.c
+++ src/startup_2_ocaml.c
@@ -36,7 +36,8 @@ void boot_primary() {
     // call ocaml land
     caml_startup(argv);
 
-    boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx ; \n comp_exceptions : 0x%lx ; \n cycles_comp_exceptions : 0x%lx ; \n cycles_hash : 0x%lx ; \n cycles_val : 0x%lx ;", read_csr(mcycle), read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
+    boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx \n comp_exceptions : 0x%lx \n cycles_comp_exceptions : 0x%lx \n cycles_hash : 0x%lx \n cycles_val : 0x%lx", read_csr(mcycle), read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
+    boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
 
     boot_printf("ocaml-boot: caml runtime returned. shutting down!\n");
 
