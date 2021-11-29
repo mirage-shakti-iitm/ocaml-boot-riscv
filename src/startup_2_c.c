517da8404c9090a2b9d283ee98bbfbfe6905448c
SaiVK <saiganesha5.svkv@gmail.com>
2021-11-29 20:28:32 +0530

    Print leastSp CSR only when running on shakti-backend

--- src/startup_2_c.c
+++ src/startup_2_c.c
@@ -37,8 +37,10 @@ void boot_primary() {
     main();
 
     boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx \n comp_exceptions : 0x%lx \n cycles_comp_exceptions : 0x%lx \n cycles_hash : 0x%lx \n cycles_val : 0x%lx", read_csr(mcycle), read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
-    boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
-    
+    #if defined(SHAKTI_UART)
+        boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
+    #endif
+        
     boot_printf("c-boot: caml runtime returned. shutting down!\n");
 
 // #ifndef UART
--- src/startup_2_mirage.c
+++ src/startup_2_mirage.c
@@ -35,7 +35,9 @@ void boot_primary() {
     riscv_boot_finished(start, mem_size);
 
     boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx \n comp_exceptions : 0x%lx \n cycles_comp_exceptions : 0x%lx \n cycles_hash : 0x%lx \n cycles_val : 0x%lx", read_csr(mcycle), read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
-    boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
+    #if defined(SHAKTI_UART)
+        boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
+    #endif
 
     boot_printf("mirage-boot: caml runtime returned. shutting down!\n");
 
