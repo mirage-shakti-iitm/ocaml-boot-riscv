a258142851c75d9991cfe033ce2e94ad88d58859
SaiVK <saiganesha5.svkv@gmail.com>
2022-04-13 22:05:36 +0530

    1. Added riscv_cycle_counter(mirage-crypto) 2. Converted j to jr instruction

--- include/ocaml-boot-riscv/compat.h
+++ include/ocaml-boot-riscv/compat.h
@@ -8,6 +8,7 @@ extern "C" {
     void riscv_poweroff(int status) __attribute__((noreturn));
     void riscv_write(const char* s, unsigned int length);
     time__t riscv_clock_monotonic();
+    time__t riscv_cycle_counter();
     void riscv_wait(time__t delay);
 
 #ifdef __cplusplus
