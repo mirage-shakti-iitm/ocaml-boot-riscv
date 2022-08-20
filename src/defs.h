b5c815cd0294e000ec91c075930a09c71854909a
SaiVK <saiganesha5.svkv@gmail.com>
2022-08-20 20:34:51 +0530

    Setting compartment enable via environment flag

--- src/defs.h
+++ src/defs.h
@@ -3,7 +3,11 @@
 
 // Compartment specific macros that should be set on entry.
 #define CHECKCAP_STACK_SIZE 4096*6
+#if defined(COMPARTMENT_ENABLE)
 #define ENABLE_CAP 0x1
+#else 
+#define ENABLE_CAP 0x0
+#endif
 #define INITIAL_COMPARTMENT 0x0
 #define Total_Compartments 256
 #define Fully_Trusted_Compartment 255
