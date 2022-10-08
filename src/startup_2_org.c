4fdbf83428b2463c72bbcf04b85cdff454fb0b55
SaiVK <saiganesha5.svkv@gmail.com>
2022-10-08 21:59:37 +0530

    Tracking mcycle alone

--- src/startup_2_org.c
+++ src/startup_2_org.c
@@ -835,9 +835,9 @@ void boot_primary() {
     #endif
 
 	boot_printf("\n \n Performance numbers : \n mcycle: 0x%lx ;", read_csr(mcycle));
-	boot_printf(" \n comp_exceptions : 0x%lx ; \n cycles_comp_exceptions : 0x%lx ; \n cycles_hash : 0x%lx ; \n cycles_val : 0x%lx ;", read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
+	// boot_printf(" \n comp_exceptions : 0x%lx ; \n cycles_comp_exceptions : 0x%lx ; \n cycles_hash : 0x%lx ; \n cycles_val : 0x%lx ;", read_csr(mhpmcounter3), read_csr(mhpmcounter4), read_csr(mhpmcounter5), read_csr(mhpmcounter6));
     #if defined(SHAKTI_UART)
-    	boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
+    	// boot_printf("\nLeast sp : 0x%x \n ",read_csr(0x80a));
     #endif
 
     boot_printf("ocaml-boot: caml runtime returned. shutting down!\n");
