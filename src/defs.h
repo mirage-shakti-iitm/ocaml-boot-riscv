3f0a8103b2c7063918e2729e6cc607bceb6178cb
SaiVK <saiganesha5.svkv@gmail.com>
2022-10-16 15:17:53 +0530

    Allow C0->C1, C1->C40 transition. And, place C0 as initial compartment

--- src/defs.h
+++ src/defs.h
@@ -8,7 +8,7 @@
 #else 
 #define ENABLE_CAP 0x0
 #endif
-#define INITIAL_COMPARTMENT 0x1
+#define INITIAL_COMPARTMENT 0x0
 #define Total_Compartments 256
 #define Fully_Trusted_Compartment 255
 #define Restricted_Compartment 254
