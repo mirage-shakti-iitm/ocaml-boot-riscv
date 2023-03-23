132521e47f2e120f15cb15da6aa28d232604d596
SaiVK <saiganesha5.svkv@gmail.com>
2023-03-23 12:39:49 +0530

    Update compartment matrix location

--- choose_compartment_strategy.sh
+++ choose_compartment_strategy.sh
@@ -2,14 +2,14 @@
 
 # option=$1
 # chosen_dir="compartment_allocations/strategy_${option}"
-cap_matrix_dir="/home/sai/Shakti-TEE-Practical/mirage-shakti/fides-cap-matrix"
+cap_matrix_dir="/home/sai/Shakti-TEE-Practical/mirage-shakti/fides-cap-matrix/fides-voting-system-1"
 src_dir="src/"
 
 # chosen_dir will contain the *(.cap/cap_tee.ml) files
 # cp ${chosen_dir}/* ${src_dir}/
 
 # generate cap_matrix.h in chosen_dir
-cap_matrix_generator ${cap_matrix_dir}/fides-voting-system-1/cap_table.txt
+cap_matrix_generator ${cap_matrix_dir}/cap_table.txt
 
 # restore original startup_2.c
 cp ${src_dir}/startup_2_org.c ${src_dir}/startup_2.c
